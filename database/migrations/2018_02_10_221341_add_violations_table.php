f3b8d607d1c3590a71b1d4b12cb0a8f76526f6ef
JJ0421 <jjgabbott@gmail.com>
2018-02-10 17:24:47 -0500

    added violations table migration

---
+++ database/migrations/2018_02_10_221341_add_violations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddViolationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('violations', function(Blueprint $table) {
+            $table->unsignedInteger('business_id');
+            $table->unsignedInteger('date');
+            $table->string('code');
+            $table->string('description');
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('violations');
+    }
+}
